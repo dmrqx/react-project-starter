4329a32a2d19ab3720e3c26094b657462dc7a621
Ju Demarque <ju.dmrq@gmail.com>
2020-06-22 16:44:17 -0300

    update all the things
    closes #1

---
+++ jest.setup.js
@@ -0,0 +1,11 @@
+import { resolve } from 'path'
+import { JSDOM } from 'jsdom'
+
+beforeEach(async () => {
+  const dom = await JSDOM.fromFile(resolve(__dirname, 'src/index.html'), null)
+  global.document.body.innerHTML = dom.window.document.body.innerHTML
+})
+
+afterEach(() => {
+  global.document.body.innerHTML = ''
+})
